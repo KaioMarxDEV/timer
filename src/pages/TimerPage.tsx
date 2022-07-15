d0ef805380bf525f7a8c9061cc3b34a100646d19
Kaio Marx <kaiomarxdev@gmail.com>
2022-07-15 06:46:46 -0300

    feat: package updates

--- src/pages/TimerPage.tsx
+++ src/pages/TimerPage.tsx
@@ -1,12 +1,35 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Play } from 'phosphor-react';
+import { useForm } from 'react-hook-form';
+import * as zod from 'zod';
+
+const newCicleFormValidationSchema = zod.object({
+  taskName: zod.string().min(1, 'Please! Type the task'),
+  taskMinutesAmount: zod.number().min(5).max(60),
+});
 
 export function TimerPage() {
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCicleFormValidationSchema),
+  });
+
+  function handleCreateNewCicle(data: any) {
+    console.log(data);
+  }
+
+  const task = watch('taskName');
+  const isSubmitDisabled = !task;
+
   return (
     <main className="flex h-full flex-1  flex-col items-center">
-      <form action="" className="flex flex-col items-center gap-14">
+      <form
+        onSubmit={handleSubmit(handleCreateNewCicle)}
+        className="flex flex-col items-center gap-14"
+      >
         <div className="flex w-full flex-wrap items-center justify-center gap-2 text-lg font-bold text-gray-100">
           <label htmlFor="task">I{"'"}m gonna work on</label>
           <input
+            {...register('taskName')}
             className="h-10 flex-1 border-0 border-b-2 border-gray-500 bg-transparent px-1 text-lg  font-bold text-gray-100 transition-all placeholder:text-gray-500 focus:-translate-y-1 focus:rounded focus:border-b-green-300 focus:shadow focus:shadow-green-300 "
             id="task"
             list="task-suggest"
@@ -14,11 +37,12 @@ export function TimerPage() {
           />
 
           <datalist id="task-suggest">
-            <option value="asdfa" />
+            <option value="dale" />
           </datalist>
 
           <label htmlFor="minutesAmount">For</label>
           <input
+            {...register('taskMinutesAmount', { valueAsNumber: true })}
             className="h-10 border-0 border-b-2 border-gray-500 bg-transparent px-1 text-lg  font-bold text-gray-100 transition-all placeholder:text-gray-500 focus:-translate-y-1 focus:rounded focus:border-b-green-300 focus:shadow focus:shadow-green-300"
             type="number"
             id="minutesAmount"
@@ -40,6 +64,7 @@ export function TimerPage() {
         </div>
         <button
           type="submit"
+          disabled={isSubmitDisabled}
           className="flex w-full max-w-2xl cursor-pointer items-center justify-center gap-2 rounded-lg border-0 bg-green-500  py-4 font-bold text-gray-300 hover:bg-green-300 hover:text-gray-100 disabled:cursor-not-allowed disabled:bg-green-700 disabled:text-gray-400"
         >
           <Play size={24} />
